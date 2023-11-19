194d647798ffab6c6db83600c07c591493c9b8d5
sady567 <138576052+sady567@users.noreply.github.com>
2023-11-19 22:39:08 +0700

    spawner enemy

--- Characters/Chichilla/chincilla.gd
+++ Characters/Chichilla/chincilla.gd
@@ -5,10 +5,22 @@ extends CharacterBody2D
 @onready var animation_sprite : AnimationPlayer = $AnimationPlayer
 @onready var sprite : Sprite2D = $Sprite2D
 
+var max_health = 100
+var health = max_health
+
+var enemy_attack_rang = false
+var enemy_attack_cooldown = true
+var player_alive = true
+
 var direction : Vector2 = Vector2.ZERO
 
 func _ready():
 	animation_sprite.play("idle_side")
+	$HealthBar.max_value = max_health
+	set_health_bar()
+
+func set_health_bar():
+	$HealthBar.value = health
 
 func handle_input():
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -20,7 +32,10 @@ func _process(delta):
 	move_and_slide()
 	animation_movement()
 	facing_direction()
-	
+	set_health_bar()
+	enemy_attack()
+
+
 func animation_movement():
 	if (direction == Vector2.LEFT):
 		animation_sprite.play("walk_side")
@@ -31,14 +46,35 @@ func animation_movement():
 	if (direction == Vector2.DOWN):
 		animation_sprite.play("walk_down")
 	if (direction == Vector2.UP):
-		animation_sprite.play("walk_down")
-		
+		animation_sprite.play("walk_up")
+
+
 func facing_direction():
 	if (direction == Vector2.LEFT):
 		sprite.flip_h = false
 	if (direction == Vector2.RIGHT):
 		sprite.flip_h = true
-	if (direction == Vector2.DOWN):
-		sprite.flip_v = false
-	if (direction == Vector2.UP):
-		sprite.flip_v = true
+	if (direction == Vector2.DOWN and Vector2.ZERO):
+		animation_sprite.play("idle_down")
+		sprite.flip_h = false
+	if (direction == Vector2.UP and Vector2.ZERO):
+		animation_sprite.play("idle_up")
+		sprite.flip_h = false
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("Goblin"):
+		enemy_attack_rang = true
+
+
+func _on_hitbox_body_exited(body):
+	if body.is_in_group("Goblin"):
+		enemy_attack_rang = false
+
+func enemy_attack():
+	if enemy_attack_rang:
+		health -= 1
+
+func player_dead():
+	if health == 0:
+		player_alive = false
--- Characters/Enemies/Goblin/goblin.gd
+++ Characters/Enemies/Goblin/goblin.gd
@@ -5,8 +5,9 @@ extends CharacterBody2D
 @onready var player = $"../Chincilla"
 @onready var motion = Vector2.ZERO
 
+@export var atk_damage : float = 15.0
 @export var goblin : CharacterBody2D
-@export var speed : float = 2.0
+@export var speed : float = 3.0
 
 func _ready():
 	animation.play("idle")
@@ -23,5 +24,10 @@ func _physics_process(delta):
 func facing_direction():
 	if (player.position.x < goblin.position.x):
 		goblin_sprite.flip_h = true
+		$Hitbox/CollisionShape2D.position.x = -11.6 
 	if (player.position.x > goblin.position.x):
 		goblin_sprite.flip_h = false
+		$Hitbox/CollisionShape2D.position.x = 11.6 
+
+func _on_hitbox_body_entered(body):
+	pass
