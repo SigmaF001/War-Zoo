52fe2f4e88045a0e3a091258a54b46a7e3425d92
sady567 <138576052+sady567@users.noreply.github.com>
2023-11-24 23:25:18 +0700

    fix bugs

--- System/enemy_spawner.gd
+++ System/enemy_spawner.gd
@@ -3,7 +3,6 @@ extends Node2D
 @export var spawns : Array[Spawn_info] = []
 
 @onready var player = get_tree().get_first_node_in_group("player")
-@onready var player_character = $"../Chincilla"
 
 var time = 0
 
